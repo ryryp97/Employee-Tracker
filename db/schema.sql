3278503d6e151b7240e5dfa97bef6c1b6e54c689
ryry <ryanpetrecca@gmail.com>
2021-01-16 19:45:13 -0800

    added a db schema and changed name

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,31 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE database employees_db;
 
--- USE employees_db;
+USE employees_db;
 
+CREATE TABLE department
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(30) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE role
+(
+	id int NOT NULL AUTO_INCREMENT,
+	title varchar(30) NOT NULL,
+    salary DECIMAL(2) NOT NULL,
+    department_id int NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id), 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE employee
+(
+    id int NOT NULL AUTO_INCREMENT,
+	first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    role_id int NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+	PRIMARY KEY (id)
+);
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 const inquirer = require("inquirer");
 const cTable = require('console.table')
 const orm = require("./config/orm.js");
+
